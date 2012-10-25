528c423e13b239a2e62cb6ef756403fb328a907e
Mikk Kirstein <mikk.kirstein@zerotech.ee>
2012-10-25 22:38:56 +0300

    Script modification

--- kickstart.sh
+++ kickstart.sh
@@ -3,35 +3,39 @@
 SCRIPTS="scripts"
 PACKAGES="packages"
 TOOL="apt-get"
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+### Setting package tool ###
+if [ "$#" -eq 1 ]
+then
+    TOOL=$1
+fi
 
 ### Install packages ###
-
 # Update && upgradepackages
-$(sudo $TOOL update --yes)
-$(sudo $TOOL upgrade --yes)
+sudo $TOOL update --yes
+sudo $TOOL upgrade --yes
 
 # loop the package listing
-list=$(cat $PACKAGES)
-for package in $list
+LIST=$(cat $PACKAGES | egrep -v "^\s*(#|$)")
+for package in $LIST
 do
-    echo $package
+    sudo $TOOL install $package --yes
 done
 
 
 ### Initiate scripts ###
-list=$(find ./$SCRIPTS -iname "*.sh")
-for file in $list
+LIST=$(find $SCRIPTS -iname "*.sh")
+for file in $LIST
 do
-    chmod +x "$file"
-    echo "running $file"
+    DIR=${file%/*}
+    FILENAME=${file##*/}
+    cd "$BASEDIR/$DIR"
+
+    chmod +x $FILENAME
+    echo "running script: $FILENAME"
+    ./$FILENAME
+
 done
-### Packages
 
-# Chrome
-#sudo apt-get install chromium-browser --yes
-
-
-
-### Misc
-# Git extras
 
--- scripts/git-extras/git-extras.sh
+++ scripts/git-extras/git-extras.sh
@@ -1,1 +1,4 @@
 curl https://raw.github.com/visionmedia/git-extras/master/bin/git-extras | INSTALL=y sh
+
+echo "Adding git aliases to bash_profile"
+cat git-aliases >> ~/.bash_profile
