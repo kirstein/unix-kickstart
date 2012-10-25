c12221529cf97ceb42a952d50719fe57a42017d2
Mikk Kirstein <mikk.kirstein@zerotech.ee>
2012-10-25 21:21:31 +0300

    script tmp

--- kickstart.sh
+++ kickstart.sh
@@ -1,15 +1,34 @@
 #!/bin/sh
 
-# Update packages
-sudo apt-get update --yes
+SCRIPTS="scripts"
+PACKAGES="packages"
+TOOL="apt-get"
 
-# Upgrade
-sudo apt-get upgrade --yes
+### Install packages ###
 
+# Update && upgradepackages
+$(sudo $TOOL update --yes)
+$(sudo $TOOL upgrade --yes)
+
+# loop the package listing
+list=$(cat $PACKAGES)
+for package in $list
+do
+    echo $package
+done
+
+
+### Initiate scripts ###
+list=$(find ./$SCRIPTS -iname "*.sh")
+for file in $list
+do
+    chmod +x "$file"
+    echo "running $file"
+done
 ### Packages
 
 # Chrome
-sudo apt-get install chromium-browser --yes
+#sudo apt-get install chromium-browser --yes
 
 
 
